43e915cd33031ed84c2771dd155a5b347498b330
syattull02 <108652288+syattull02@users.noreply.github.com>
2023-09-18 14:25:29 +0800

    Create chicken.php

---
+++ chicken.php
@@ -0,0 +1,13 @@
+<html>
+<h1>SYA'S CAFE</h1>
+    <ul>
+        <li> <a href="login.php">LOGIN</a></li>
+        <li> <a href="paparanmenu.php">MENU</a></li>
+        <li> <a href="order.php">ORDER</a></li>
+    </ul>
+    <img src="chicken1.jpg" alt="" srcset="">
+    Korean Chicken <br>
+    Rm 15.00/5pcs
+    <br>
+    <input type="submit" value="order" name="order">
+</html>
